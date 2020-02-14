baf7c293499a2bf7482a5575cf340151ee336d1e
Josh Long <josh@joshlong.com>
2020-02-13 23:04:42 -0800

    initial commit

---
+++ src/test/java/com/example/cloudfoundryjavaclientautoconfiguration/CloudfoundryJavaClientAutoconfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudfoundryjavaclientautoconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudfoundryJavaClientAutoconfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
