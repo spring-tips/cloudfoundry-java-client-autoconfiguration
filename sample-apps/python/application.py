68080e6a1f08797fe7468da4002a6837fa88a8b0
Josh Long <josh@joshlong.com>
2020-02-14 03:08:32 -0800

    polishing the python app

--- sample-apps/python/application.py
+++ sample-apps/python/application.py
@@ -4,14 +4,12 @@ import os
 
 app = Flask(__name__)
 cf_port = os.getenv("PORT")
+if cf_port is None:
+    cf_port = '5000'
 
 @app.route('/')
 def hello():
     return {'message': 'Hello, world'}
 
-
 if __name__ == '__main__':
-    if cf_port is None:
-        app.run(host='0.0.0.0', port=5000, debug=True)
-    else:
-        app.run(host='0.0.0.0', port=int(cf_port), debug=True)
+    app.run(host='0.0.0.0', port=int(cf_port), debug=True)
